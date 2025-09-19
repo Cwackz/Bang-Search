ca683862057289a7d168675af01b67f766113faf
Benjamin <Benjaminmk06@gmail.com>
2025-09-19 12:23:28 +0200

    Initial Commit

---
+++ Bang Search/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Bang Search
+//
+//  Created by Benjamin Melchior-Kongsmar on 19/09/2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
